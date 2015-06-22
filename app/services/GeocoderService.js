5be344c387463f700ef38b8a2f7a69a898a4ec22
Éverton Inocêncio <hewerthomn@gmail.com>
2015-06-21 22:23:48 -0400

    criado geocoder service

---
+++ app/services/GeocoderService.js
@@ -0,0 +1,36 @@
+(function(angular, undefined) {
+	'use strict';
+
+	/**
+	 * Geocoder Service
+	 */
+	angular
+		.module('app')
+		.service('Geocoder', GeocoderService);
+
+	function GeocoderService($http) {
+
+		this.searchPlace = function(query) {
+			return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
+				params: {
+					address: query,
+					sensor: false
+				}
+			});
+		};
+
+		this.getPlaceInfo = function(lonlat) {
+			return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
+				params: {
+					latlng: lonlat.lat + ',' + lonlat.lon,
+					sensor: false
+				}
+			});
+		};
+
+		this.getGeoIP = function() {
+			return $http.jsonp('http://www.telize.com/geoip?callback=JSON_CALLBACK');
+		};
+	}
+
+})(window.angular);
