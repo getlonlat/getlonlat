c377bfbe2b089a191f0ddcb6ff349a267b020189
Éverton Inocêncio <hewerthomn@gmail.com>
2015-06-21 21:37:45 -0400

    adicionado jit-grunt e grunt-notify; adicionado em bloco iife

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,91 +1,100 @@
-module.exports = function(grunt) {
+(function(undefined) {
+	module.exports = function(grunt) {
 
-	require('time-grunt')(grunt);
-
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		concat: {
-			options: {
-				separator: ';',
-				stripBanners: { block: true, line: true },
-			},
-			app: {
-				dest: 'build/js/app.min.js',
-				src: [
-					'vendor/jquery/dist/jquery.js',
-					'vendor/bootstrap/dist/js/bootstrap.js',
-					'vendor/angular/angular.js',
-					'vendor/angular-touch/angular-touch.js',
-					'vendor/angular-focus-it/angular-focus-it.js',
-					'vendor/angular-route/angular-route.js',
-
-					'app/app.js',
-					'app/config.js',
-					'app/routes.js',
-					'app/components/home/HomeController.js',
-					'app/services/MapService.js',
-				]
-			}
-		},
-
-		copy: {
-			fonts: {
-				files: [
-					{
-						expand: true,
-						flatten: true,
-						src: ['vendor/font-awesome/fonts/**.*'],
-						dest: 'build/fonts'
-					}
-				]
-			}
-		},
-
-		uglify: {
-			app: {
+		grunt.initConfig({
+			pkg: grunt.file.readJSON('package.json'),
+			concat: {
 				options: {
-					mangle: false
+					separator: ';',
+					stripBanners: { block: true, line: true },
 				},
-				files: {
-					'build/js/app.min.js': ['build/js/app.min.js']
-				}
-			}
-		},
+				app: {
+					dest: 'build/js/app.min.js',
+					src: [
+						'vendor/jquery/dist/jquery.js',
+						'vendor/bootstrap/dist/js/bootstrap.js',
+						'vendor/angular/angular.js',
+						'vendor/angular-touch/angular-touch.js',
+						'vendor/angular-focus-it/angular-focus-it.js',
+						'vendor/angular-route/angular-route.js',
 
-		cssmin: {
-			combine: {
-				files: {
-					'build/css/app.min.css': [
-						'css/bootstrap-united.css',
-						'vendor/font-awesome/css/font-awesome.css',
-						'css/app.css',
+						'app/app.js',
+						'app/config.js',
+						'app/routes.js',
+						'app/components/home/HomeController.js',
+						'app/services/MapService.js',
 					]
 				}
-			}
-		},
+			},
 
-		watch: {
-			dev: {
-				files: ['Gruntfile.js', 'app/**/*.js', 'css/**/*.css'],
-				tasks: ['concat:app', 'cssmin'],
-				options: {
-					atBegin: true,
-					liveReload: true
+			copy: {
+				fonts: {
+					files: [
+						{
+							expand: true,
+							flatten: true,
+							src: ['vendor/font-awesome/fonts/**.*'],
+							dest: 'build/fonts'
+						}
+					]
 				}
 			},
-		},
 
-		jshint: {
-			all: ['Gruntfile.js', 'app/**/*.js']
-		}
-	});
+			uglify: {
+				app: {
+					options: {
+						mangle: false
+					},
+					files: {
+						'build/js/app.min.js': ['build/js/app.min.js']
+					}
+				}
+			},
 
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+			cssmin: {
+				combine: {
+					files: {
+						'build/css/app.min.css': [
+							'css/bootstrap-united.css',
+							'vendor/font-awesome/css/font-awesome.css',
+							'css/app.css',
+						]
+					}
+				}
+			},
 
-	grunt.registerTask('dev', ['concat', 'cssmin']);
-	grunt.registerTask('default', ['concat',  'uglify', 'cssmin', 'copy:fonts']);
-};
+			watch: {
+				dev: {
+					files: ['Gruntfile.js', 'app/**/*.js', 'css/**/*.css'],
+					tasks: ['concat:app', 'cssmin'],
+					options: {
+						atBegin: true,
+						liveReload: true
+					}
+				},
+			},
+
+			jshint: {
+				all: ['Gruntfile.js', 'app/**/*.js']
+			},
+
+		  notify_hooks: {
+		    options: {
+		      enabled: true,
+		      success: true,
+		      max_jshint_notifications: 5
+		    }
+		  }
+		});
+
+		grunt.loadNpmTasks('grunt-notify');
+		grunt.task.run('notify_hooks');
+
+		grunt.registerTask('dev', ['concat', 'cssmin']);
+		grunt.registerTask('default', ['concat',  'uglify', 'cssmin', 'copy:fonts']);
+
+		require('time-grunt')(grunt);
+		require('jit-grunt')(grunt);
+	};
+
+})();
